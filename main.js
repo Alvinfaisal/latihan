8b673c8b0396cdb0d159081415745e90056ea661
Ricky-yuu <meliodaskirisami@gmail.com>
2021-07-24 12:24:41 +0700

    tambah js

---
+++ main.js
@@ -0,0 +1,19 @@
+
+        function imgSlider(anything) {
+            document.querySelector('.starbucks').src = anything;
+        }
+
+        function changeCircleColor(color) {
+            const circle = document.querySelector('.circle');
+            circle.style.background = color;
+        }
+
+        function changeSpanColor(color) {
+            const warna = document.querySelector('.warna');
+            warna.style.color = color;
+        }
+
+        function changeButtonColor(color) {
+            const btn = document.querySelector('.btn');
+            btn.style.background = color;
+        }
